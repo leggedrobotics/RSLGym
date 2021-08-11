72ffc20fbf78eaa014025bac9ecebc2c7d30c7b6
Pascal Egli <pasegli@ethz.ch>
2021-08-11 09:11:42 +0200

    parallelize setInfo();

--- rslgym/wrapper/include/VectorizedEnvironment.hpp
+++ rslgym/wrapper/include/VectorizedEnvironment.hpp
@@ -1,27 +1,3 @@
-//
-// Created by jemin on 3/27/19.
-// MIT License
-//
-// Copyright (c) 2019-2019 Robotic Systems Lab, ETH Zurich
-//
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-//
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-//
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 #ifndef SRC_RAISIMGYMVECENV_HPP
 #define SRC_RAISIMGYMVECENV_HPP
 
@@ -39,12 +15,15 @@ class VectorizedEnvironment {
   explicit VectorizedEnvironment(std::string resourceDir, std::string cfg_str)
       : resourceDir_(resourceDir) {
     cfg_ = YAML::Load(cfg_str);
-    raisim::World::setActivationKey(raisim::Path(resourceDir + "/activation.raisim").getString());
   }
 
   ~VectorizedEnvironment() {
     for (auto *ptr: environments_)
       delete ptr;
+  }
+
+  void setActivationKey(std::string path) {
+    raisim::World::setActivationKey(raisim::Path(path).getString());
   }
 
   void init() {
@@ -169,7 +148,7 @@ class VectorizedEnvironment {
   }
 
   void setInfo(std::unordered_map<std::string, Eigen::Ref<EigenRowMajorMat>> &info) {
-// #pragma omp parallel for schedule(dynamic)
+ #pragma omp parallel for schedule(static)
     for (int i = 0; i < num_envs_; i++) {
       std::unordered_map<std::string, EigenVec> info_i;
       for (auto& kv : info) {
